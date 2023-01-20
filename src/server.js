c42f3a74fa4f41137d88f263097fab68a08a6535
Gabriel Vitebo <vitebo@outlook.com>
2023-01-20 12:31:46 -0300

    using query params

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 
 const app = express()
@@ -5,6 +6,12 @@ const app = express()
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params
   response.send(`id da mensagem ${id} Para o usuário ${user}`)
+})
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página: ${page} Mostrar: ${limit}`)
 })
 
 const PORT = 3333
