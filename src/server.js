fca2648bfe68af48d4f171ce5f71e93d6776da06
Gabriel Vitebo <vitebo@outlook.com>
2023-01-20 12:18:00 -0300

    using the GET method

--- src/server.js
+++ src/server.js
@@ -2,5 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params
+  response.send(`id da mensagem ${id} Para o usuário ${user}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
