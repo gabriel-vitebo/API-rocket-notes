a26fb98a74e4497be74d4898aaa7161d71f8b633
Gabriel Vitebo <vitebo@outlook.com>
2023-01-24 16:30:56 -0300

    encrypting the password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
-
 const sqliteConnection = require('../database/sqlite')
 
 
@@ -13,6 +13,12 @@ class UsersController {
     if(checkUserExist) {
       throw new AppError('Este email já está cadastrado')
     }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]
+    );
 
     return response.status(201).json()
   }
--- src/server.js
+++ src/server.js
@@ -27,7 +27,5 @@ app.use(( error, request, response, next ) => {
   })
 })
 
-
-
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
