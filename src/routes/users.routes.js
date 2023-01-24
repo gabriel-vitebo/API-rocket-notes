f3540b906140b927a0f212bdbcc9e8986c055e4c
Gabriel Vitebo <vitebo@outlook.com>
2023-01-24 17:11:00 -0300

    updating user name and email

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRouter = Router()
 const usersController = new UsersController()
 
 usersRouter.post("/", usersController.create)
+usersRouter.put("/:id", usersController.update)
 
 module.exports = usersRouter
