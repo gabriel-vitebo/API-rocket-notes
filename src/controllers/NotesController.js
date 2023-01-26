9320b43b58a8fd47a8c173b3ee4510fefac3ae07
Gabriel Vitebo <vitebo@outlook.com>
2023-01-26 15:53:21 -0300

    finalizing the node.js back-end module

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -81,8 +81,17 @@ class NotesController{
        .orderBy("title")
     }
 
+    const userTags = await knex("tags").where({ user_id })
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
 
-    return response.json(notes)
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
   }
 }
 
