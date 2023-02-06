c96e54fc2ab3347e31d71ca368d5f6c55802ddc9
Gabriel Vitebo <vitebo@outlook.com>
2023-02-06 15:09:00 -0300

    loading images

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,17 +1,17 @@
 const path = require("path")
 const multer = require("multer")
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads" )
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads" )
 const crypto = require("crypto")
 
 const MULTER = {
-  Storage: multer.diskStorage({
+  storage: multer.diskStorage({
     destination: TMP_FOLDER,
-    filename(request, filename, callback) {
+    filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString("hex")
       const fileName = `${fileHash}-${file.originalname}`
 
-      return callback(null, filename)
+      return callback(null, fileName)
     }
   })
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
 const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const usersRouter = Router()
+upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
 
 usersRouter.post("/", usersController.create)
 usersRouter.put("/", ensureAuthenticated, usersController.update)
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename)
+  response.json()
+})
 
 module.exports = usersRouter
