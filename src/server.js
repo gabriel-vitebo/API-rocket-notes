73c8eb5e6c72e388cd8170c669c425d85179aead
Gabriel Vitebo <vitebo@outlook.com>
2023-02-06 15:42:23 -0300

    installing cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
-const express = require('express')
 
+const cors = require("cors")
+const express = require('express')
 const routes = require('./routes')
 
 migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
