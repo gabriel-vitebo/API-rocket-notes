6575a55dd4440d12efc24a58578df5739b86e369
Gabriel Vitebo <vitebo@outlook.com>
2023-02-09 18:02:44 -0300

    adding control to bounce the photo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const cors = require("cors")
 const express = require('express')
@@ -10,6 +11,7 @@ migrationsRun()
 
 const app = express()
 app.use(cors())
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(express.json())
 app.use(routes)
 
