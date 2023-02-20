38b0f50130622a2d54d4177f79969f3bad1fc4f4
gabriel-vitebo <vitebo@outlook.com>
2023-02-20 15:40:17 -0300

    configuring jest and separating functions

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,16 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
+
+const UserRepository = require("../repositories/UserRepository")
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response) {
-    console.log("Chegou aqui")
     const { name, email, password } = request.body
 
-    const database = await sqliteConnection()
-    const checkUserExist = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    )
+    const userRepository = new UserRepository()
+
+    const checkUserExist = await userRepository.findByEmail(email)
 
     if (checkUserExist) {
       throw new AppError("Este email já está cadastrado")
@@ -19,10 +18,7 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8)
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    )
+    await userRepository.create({ name, email, password: hashedPassword })
 
     return response.status(201).json()
   }
