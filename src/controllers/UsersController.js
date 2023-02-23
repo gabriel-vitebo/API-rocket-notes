9886c2d8ffd4575421070588a3ef41ed5cd74418
gabriel-vitebo <vitebo@outlook.com>
2023-02-23 11:56:18 -0300

    creating dependency inversion

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,22 +3,15 @@ const AppError = require("../utils/AppError")
 
 const UserRepository = require("../repositories/UserRepository")
 const sqliteConnection = require("../database/sqlite")
+const UserCreateService = require("../services/UserCreateService")
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
     const userRepository = new UserRepository()
-
-    const checkUserExist = await userRepository.findByEmail(email)
-
-    if (checkUserExist) {
-      throw new AppError("Este email já está cadastrado")
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await userRepository.create({ name, email, password: hashedPassword })
+    const userCreateService = new UserCreateService(userRepository)
+    await userCreateService.execute({ name, email, password })
 
     return response.status(201).json()
   }
